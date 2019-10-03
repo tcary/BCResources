33f76d9b1f581632c4947a412f5574c81e0db15d
Tetiana Cary <taniushka.tata@gmail.com>
2019-10-03 12:42:32 -0600

    Added more links and final touches.

--- assets/script.js
+++ assets/script.js
@@ -612,12 +612,456 @@ var links = [
     {
         href: "https://www.youtube.com/watch?v=EGhF4iJSnl0",
         text: "Stopwatch Video Walkthrough: ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=oTart7fFefI",
+        text: "FizzBuzz Video Walkthrough: ",
+        category: "javascript"
+    },
+    {
+        href: "https://du.bootcampcontent.com/denver-coding-bootcamp/DU-DEN-FSF-FT-08-2019-U-C/tree/master/Algorithms",
+        text: "Practice the JS Algorithms (try to tackle 10-17 this week!)  ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.digitalocean.com/community/tutorials/using-while-and-do-while-loops-in-javascript",
+        text: "Using While and Do...While Loops in JavaScript: ",
+        category: "javascript"
+    },
+    {
+        href: "https://visualgo.net/en/sorting",
+        text: "VisuAlgo (neat and helpful algorithm visualizations): ",
+        category: "javascript"
+    },
+    {
+        href: "https://rapidapi.com/",
+        text: "RapidAPI Marketplace: ",
+        category: "api"
+    },
+    {
+        href: "http://osp123.github.io/tutorials/html/weatherAPI.html",
+        text: "Wanna get some practice with APIs? Try this tutorial! ",
+        category: "api"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=t-qAWbYMiUs",
+        text: "Bubble Sort Video Walkthrough: ",
+        category: "javascript"
+    },
+    {
+        href: "https://du.bootcampcontent.com/denver-coding-bootcamp/DU-DEN-FSF-FT-08-2019-U-C/blob/master/Video-Guides/Unit-5.md",
+        text: "Unit 5 Video Guide (Stopwatch, FizzBuzz, Bubble Sort):  ",
+        category: "javascript"
+    },
+    {
+        href: "https://developers.giphy.com/docs/",
+        text: "GIPHY API Documentation <--- you’ll need this for the homework!  ",
+        category: "api"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=ovYORLkO5bQ",
+        text: "HueCraft (Minecraft + Philips Hue)(neat example of using an API to interact with physical devices): ",
+        category: "api"
+    },
+    {
+        href: "http://osp123.github.io/tutorials/html/weatherAPI.html",
+        text: "Tutorial on using a weather API: ",
+        category: "api"
+    },
+    {
+        href: "https://www.w3schools.com/js/js_reserved.asp",
+        text: "JavaScript Reserved Keywords: ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.w3schools.com/js/js_json_stringify.asp",
+        text: "JSON.stringify(): ",
         category: "jquery"
     },
     {
-        href: "",
-        text: " ",
-        category: ""
+        href: "https://www.w3schools.com/jquery/event_preventdefault.asp",
+        text: "event.preventDefault(): ",
+        category: "jquery"
+    },
+    {
+        href: "https://makandracards.com/makandra/39543-jquery-promises-done-and-then-are-not-the-same",
+        text: "jQuery promises: .then() and .done() are not the same: ",
+        category: "jquery"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=Kp7Xy2LScLM",
+        text: "GIPHY API Basics Video Walkthrough: ",
+        category: "api"
+    },
+    {
+        href: "https://javascript-workbook.netlify.com/",
+        text: "Want to practice the JS basics? Try the JavaScript Workbook: ",
+        category: "javascript"
+    },
+    {
+        href: "http://api.jquery.com/attr/#setting-several-attrs",
+        text: "Adding multiple attributes at once using jQuery's .attr(): ",
+        category: "jquery"
+    },
+    {
+        href: "http://api.jquery.com/jquery.param/",
+        text: "jQuery .param() (we used this in the NYTimes solution to build a query string from an object):  ",
+        category: "jquery"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=K1JDUkF94cs",
+        text: "Button-Triggered AJAX Activity Video Walkthrough: ",
+        category: "jquery"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=UVBmX4cZkHY",
+        text: "Dynamic Element Generation with jQuery/AJAX Video Walkthrough: ",
+        category: "jquery"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=QiIlhmeGYuk",
+        text: "NYTimes Article Search App Video Walkthrough (I know the solution is a little obtuse, but try to work through it alongside the video!): ",
+        category: "jquery"
+    },
+    {
+        href: "https://du.bootcampcontent.com/denver-coding-bootcamp/DU-DEN-FSF-FT-08-2019-U-C/blob/master/Video-Guides/Unit-6.md",
+        text: "Unit 6 Video Guide (AJAX and APIs, including NYTimes App):  ",
+        category: "api"
+    },
+    {
+        href: "https://scotch.io/@PratyushB/local-storage-vs-session-storage-vs-cookie",
+        text: "Local Storage vs Session Storage vs Cookies: ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.whoishostingthis.com/resources/cookies-guide/",
+        text: "Browser Cookies: What Are They & Why Should You Care? ",
+        category: "javascript"
+    },
+    {
+        href: "https://blog.logrocket.com/the-complete-guide-to-using-localstorage-in-javascript-apps-ba44edb53a36/",
+        text: "The Complete Guide to Using localStorage in JavaScript Apps: ",
+        category: "javascript"
+    },
+    {
+        href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice",
+        text: "Array .splice() (not to be confused with .slice(), this allows you to either add or remove items from specific indexes in an array):  ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=0PHeP5bLqYE",
+        text: "Click Countdown Video Walkthrough: ",
+        category: "jquery"
+    },
+    {
+        href: "https://atendesigngroup.com/blog/array-map-filter-and-reduce-js",
+        text: "Array Map, Filter and Reduce in JS: ",
+        category: "javascript"
+    },
+    {
+        href: "https://codeburst.io/javascript-arrow-functions-for-beginners-926947fc0cdc",
+        text: "Arrow Functions for Beginners: ",
+        category: "javascript"
+    },
+    {
+        href: "https://firebase.google.com/docs/database/admin/save-data",
+        text: "Firebase .set() vs .push() vs .update(): ",
+        category: "firebase"
+    },
+    {
+        href: "https://firebase.google.com/docs/reference/js/firebase.database.DataSnapshot#exists",
+        text: "Firebase .exists() method: ",
+        category: "firebase"
+    },
+    {
+        href: "https://firebase.google.com/docs/reference/js/firebase.database.DataSnapshot#numChildren",
+        text: "Firebase .numChildren() method: ",
+        category: "firebase"
+    },
+    {
+        href: "https://firebase.google.com/docs/firestore/solutions/presence#solution_cloud_functions_with_realtime_database",
+        text: "Using Presence in Firebase: ",
+        category: "firebase"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=ZWH19t4ujRA",
+        text: "Firebase Most Recent User Video Walkthrough: ",
+        category: "firebase"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=jpSjBZKnrN4",
+        text: "Coder's Bay Video Walkthrough: ",
+        category: "firebase"
+    },
+    {
+        href: "https://du.bootcampcontent.com/denver-coding-bootcamp/DU-DEN-FSF-FT-08-2019-U-C/blob/master/Class-Content/07-firebase/01-Activities/20-MomentJS/momentjs-livecoded.html",
+        text: "Livecoded Moment.js Activity from Today: ",
+        category: "javascript"
+    },
+    {
+        href: "http://momentjs.com/",
+        text: "Moment.js... (manipulate time!): ",
+        category: "javascript"
+    },
+    {
+        href: "https://en.wikipedia.org/wiki/Unix_time",
+        text: "Unix/Epoch Time: ",
+        category: "javascript"
+    },
+    {
+        href: "https://du.bootcampcontent.com/denver-coding-bootcamp/DU-DEN-FSF-FT-08-2019-U-C/blob/master/Resources/Git/git-collaboration-workflow.md",
+        text: "Recommended Workflow for Git Team Collaboration (bookmark me!): ",
+        category: "github"
+    },
+    {
+        href: "https://www.freecodecamp.org/news/what-exactly-is-node-js-ae36e97449f5/",
+        text: "What Exactly is Node.js: ",
+        category: "node"
+    },
+    {
+        href: "https://medium.com/prod-io/understanding-the-basics-of-node-js-99e01c5d844f",
+        text: "Understanding the basics of Node.js (concepts, pros, cons): ",
+        category: "node"
+    },
+    {
+        href: "https://codeburst.io/process-env-what-it-is-and-why-when-how-to-use-it-effectively-505d0b2831e7",
+        text: "process.env: What it is and why/when/how to use it effectively: ",
+        category: "node"
+    },
+    {
+        href: "https://www.digitalocean.com/community/tutorials/how-to-use-the-switch-statement-in-javascript",
+        text: "How to Use a Switch Statement in JavaScript: ",
+        category: "javascript"
+    },
+    {
+        href: "https://codeburst.io/javascript-what-are-template-literals-5d08a50ef2e3",
+        text: "JavaScript: What Are Template Literals? ",
+        category: "javascript"
+    },
+    {
+        href: "https://css-tricks.com/template-literals/",
+        text: "More Info on Template Literals: ",
+        category: "css"
+    },
+    {
+        href: "https://sass-lang.com/guide",
+        text: "SASS (Syntactically Awesome Style Sheets) Basics: ",
+        category: "css"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=JH28RCouqfw",
+        text: "Node Calculator Video Walkthrough (<<< watch it and code it again!): ",
+        category: "node"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=8aGhZQkoFbQ&t=348s",
+        href: "https://www.youtube.com/watch?v=cCOL7MC4Pl0&t=117s",
+        text: "JavaScript Event Loop: ",
+        category: "javascript"
+    },
+    {
+        href: "https://stackoverflow.com/questions/43100094/three-gray-dots-under-variable-names-in-visual-studio",
+        text: "Three gray dots under variable names in Visual Studio ",
+        category: "node"
+    },
+    {
+        href: "https://www.sitepoint.com/understanding-module-exports-exports-node-js/",
+        text: "Understanding module.exports and exports in Node.js: ",
+        category: "node"
+    },
+    {
+        href: "https://medium.freecodecamp.org/javascript-modules-a-beginner-s-guide-783f7d7a5fcc",
+        text: "JavaScript Modules: A Beginner’s Guide: ",
+        category: "javascript"
+    },
+    {
+        href: "https://nodejs.org/api/fs.html",
+        text: "Node FS (File System) Documentation: ",
+        category: "node"
+    },
+    {
+        href: "https://docs.nodejitsu.com/articles/file-system/how-to-read-files-in-nodejs/",
+        text: "How do I read files in Node: ",
+        category: "node"
+    },
+    {
+        href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises",
+        text: "Using Promises (like .then() and .catch()):  ",
+        category: "node"
+    },
+    {
+        href: "https://www.codingame.com/playgrounds/347/javascript-promises-mastering-the-asynchronous/the-catch-method",
+        text: "The .catch() method: ",
+        category: "node"
+    },
+    {
+        href: "https://en.wikipedia.org/wiki/UTF-8",
+        text: "UTF-8 Info: ",
+        category: "node"
+    },
+    {
+        href: "https://www.w3schools.com/jsref/jsref_tofixed.asp",
+        text: "JavaScript .toFixed() (allows you to specify the number of decimal points on a number):  ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.npmjs.com/package/axios",
+        text: "Axios npm package (what we use to mimic an AJAX-like functionality on the back-end):  ",
+        category: "node"
+    },
+    {
+        href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice",
+        text: "Array .slice() method (use this to make a partial or full copy of an array, ie use process.argv.slice(2) to get an array with ONLY user input):  ",
+        category: "node"
+    },
+    {
+        href: "https://www.w3schools.com/js/js_array_sort.asp",
+        text: "JavaScript .sort() array method (with info on compare functions to sort numerically):  ",
+        category: "javascript"
+    },
+    {
+        href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Description",
+        text: "More info on the .sort() compare method we used this morning: ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.w3schools.com/jsref/jsref_obj_array.asp",
+        text: "JavaScript array method reference: ",
+        category: "javascript"
+    },
+    {
+        href: "https://github.com/lnchapin/arraymethods",
+        text: "Lindsay’s array method practice activities: ",
+        category: "javascript",
+        category: "node"
+    },
+    {
+        href: "https://medium.com/coinmonks/everything-you-wanted-to-know-about-package-lock-json-b81911aa8ab8",
+        text: "Everything You Wanted To Know About package-lock.json But Were Too Afraid To Ask: ",
+        category: "node"
+    },
+    {
+        href: "https://www.twilio.com/blog/2017/08/working-with-environment-variables-in-node-js.html",
+        text: "Working with Environment Variables in Node.js (gives you more insight into .env/keys for the Liri HW):  ",
+        category: "node"
+    },
+    {
+        href: "https://www.npmjs.com/package/inquirer",
+        text: "Inquirer Documentation: ",
+        category: "node"
+    },
+    {
+        href: "https://searchcode.com/codesearch/view/98875892/",
+        text: "Inquirer Example Code (includes examples with  validate and when):  ",
+        category: "node"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=JJqriV7Q9og",
+        text: "Building Command Line CLIs with Inquirer.js Video Walkthrough: ",
+        category: "node"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=G9CtacWgYho",
+        text: "Node Geocoder Video Walkthrough: ",
+        category: "node"
+    },
+    {
+        href: "https://developers.google.com/web/tools/chrome-devtools/javascript/",
+        text: "Get Started with Debugging JavaScript in Chrome DevTools: ",
+        category: "javascript"
+    },
+    {
+        href: "https://code.visualstudio.com/docs/editor/debugging",
+        text: "Debugging in VSCode: ",
+        category: "html",
+        category: "javascript",
+        category: "node"
+    },
+    {
+        href: "https://raygun.com/javascript-debugging-tips",
+        text: "The 14 JavaScript debugging tips you probably didn’t know: ",
+        category: "javascript"
+    },
+    {
+        href: "https://medium.com/appsflyer/10-tips-for-javascript-debugging-like-a-pro-with-console-7140027eb5f6",
+        text: "10 Tips for Javascript Debugging Like a PRO with Console: ",
+        category: "javascript"
+    },
+    {
+        href: "http://juliepagano.com/blog/2014/05/18/javascript-debugging-for-beginners/",
+        text: "JavaScript Debugging for Beginners: ",
+        category: "javascript"
+    },
+    {
+        href: "https://www.digitalocean.com/community/tutorials/using-while-and-do-while-loops-in-javascript",
+        text: "Using While and Do...While Loops in JavaScript: ",
+        category: "node"
+    },
+    {
+        href: "https://www.thecodeship.com/web-development/methods-within-constructor-vs-prototype-in-javascript/",
+        text: "Methods Within Constructor vs Prototype in Javascript: ",
+        category: "node"
+    },
+    {
+        href: "https://tylermcginnis.com/beginners-guide-to-javascript-prototype/",
+        text: "A Beginner’s Guide to JavaScript’s Prototype: ",
+        category: "node"
+    },
+    {
+        href: "https://inventwithpython.com/blog/2014/12/02/why-is-object-oriented-programming-useful-with-a-role-playing-game-example/",
+        text: "Why is Object-Oriented Programming Useful? (With a Role Playing Game Example): ",
+        category: "node"
+    },
+    {
+        href: "https://medium.freecodecamp.org/recursion-recursion-recursion-4db8890a674d",
+        text: "Recursion, Recursion, Recursion: ",
+        category: "node"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=6_Qi4yg8jQg",
+        text: "Character Create Video Walkthrough: ",
+        category: "node"
+    },
+    {
+        href: "https://www.youtube.com/watch?v=Ph519qG6b9Qc",
+        text: "Programmer Loop Video Walkthrough: ",
+        category: "node"
+    },
+    {
+        href: "https://codeburst.io/javascript-wtf-is-es6-es8-es-2017-ecmascript-dca859e4821c",
+        text: "JavaScript — WTF is ES6, ES8, ES 2017, ECMAScript… ? ",
+        category: "node"
+    },
+    {
+        href: "https://codeburst.io/javascript-arrow-functions-for-beginners-926947fc0cdc",
+        text: "JavaScript Arrow Functions for Beginners: ",
+        category: "javascript"
+    },
+    {
+        href: "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75",
+        text: "JavaScript ES6+: var, let, or const? ",
+        category: "javascript"
+    },
+    {
+        href: "https://wesbos.com/destructuring-objects/",
+        text: "A Dead Simple intro to Destructuring JavaScript Objects: ",
+        category: "javascript"
+    },
+    {
+        href: "https://atendesigngroup.com/blog/array-map-filter-and-reduce-js",
+        text: "Array .map(), .filter(), and .reduce(): ",
+        category: "javascript"
+    },
+    {
+        href: "https://babeljs.io/en/repl",
+        text: "Babel Repl (a site that allows you to paste in newer JS syntax and see how it transpiles to ES5 JS): ",
+        category: "javascript"
+    },
+    {
+        href: "https://caniuse.com/",
+        text: "caniuse.com (site that allows you to search for a JS/HTML/CSS convention to see its browser support): ",
+        category: "html"
     },
     {
         href: "",
@@ -629,76 +1073,8 @@ var links = [
         text: " ",
         category: ""
     },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
-    {
-        href: "",
-        text: " ",
-        category: ""
-    },
+
+
 
 
 
@@ -712,15 +1088,16 @@ $(document).ready(function start() {
     $("#show").hide()
 })
 
-$(".col").click(function () {
+$(".col").on("click", "button", function () {
 
     $(".hide").hide();
     $("#show").show();
+    console.log($(this));
     var topic = $(this).attr("data-name");
     console.log(topic);
-    if (topic == links.category) {
-        $(".buttons-view").empty();
-        for (var i = 0; i < links[i].category.length; i++) {
+    $(".buttons-view").empty();
+    for (var i = 0; i < links.length; i++) {
+        if (topic == links[i].category) {
 
             var p = $("<p>")
             var anchor = $("<a>");
